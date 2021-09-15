45034e5b401430ad9614775145b0e878658c1e2f
Denissychev1 <sda66@mail.ru>
2021-09-15 22:54:04 +0300

    put

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -46,6 +46,6 @@ class TodosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def todo_params
-      params.require(:todo).permit(:text, :title_id)
+      params.require(:todo).permit(:text, :title_id, :checked)
     end
 end
